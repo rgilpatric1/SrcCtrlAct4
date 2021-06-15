27a12f1177f0de3c962a7d63e145abe8a7cac8d9
Mitchell <krasma423@gmail.com>
2021-06-15 14:02:37 -0500

    Mitchell Description #2

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -7,9 +7,9 @@ namespace Activity4
         static void Main(string[] args)
         {
             Console.WriteLine("Kevin Niu - Brookfield Central - N/A");
-            Console.WriteLine("Sumanth Kota - Brookfield Central High School - Panera Bread Gift Card");
+            Console.WriteLine("Sumanth Kota - Brookfield Central High School");
             Console.WriteLine("Benjamin Kim -Brookfield Central - Plastic Bag");
-            Console.WriteLine("Mitchell Anderson - New Berlin West - Socks");
+            Console.WriteLine("Mitchell Anderson - New Berlin West - Socks - High School Diploma");
         }
     }
 }
