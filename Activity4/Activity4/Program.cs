8c6f75a9a28e9b4994dde2a97124dd3c0e8a018a
Kevin Niu <kevin.h.niu@gmail.com>
2021-06-15 14:00:37 -0500

    Kevin's New Changes

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -6,9 +6,9 @@ namespace Activity4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kevin Niu - Brookfield Central - N/A");
+            Console.WriteLine("Kevin Niu - Brookfield Central - N/A - Money");
             Console.WriteLine("Sumanth Kota - Brookfield Central High School - Panera Bread Gift Card");
-            Console.WriteLine("Benjamin Kim -Brookfield Central - Plastic Bag");
+            Console.WriteLine(" - Brookfield Central - Plastic Bag");
             Console.WriteLine("Mitchell Anderson - New Berlin West - Socks");
         }
     }
