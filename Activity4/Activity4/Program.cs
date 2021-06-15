94f4ef03f3f630e8348506ec2e3fcd7623fbba54
Mitchell <krasma423@gmail.com>
2021-06-15 13:55:47 -0500

    Mitchell Anderson Description

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -9,6 +9,7 @@ namespace Activity4
             Console.WriteLine("Kevin Niu - Brookfield Central - N/A");
             Console.WriteLine("Sumanth Kota - Brookfield Central High School - Panera Bread Gift Card");
             Console.WriteLine("Benjamin Kim -Brookfield Central - Plastic Bag");
+            Console.WriteLine("Mitchell Anderson - New Berlin West - Socks");
         }
     }
 }
